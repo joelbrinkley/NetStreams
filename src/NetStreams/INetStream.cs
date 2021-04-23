6375dc55ceecdaf79b0cfb71693229b3133e6cfc
Joel Brinkley <jvbrinkley21@gmail.com>
2021-04-23 08:19:40 -0500

    feat: handle and handle async support (#21)

--- src/NetStreams/INetStream.cs
+++ src/NetStreams/INetStream.cs
@@ -10,7 +10,9 @@ namespace NetStreams
         INetStreamConfigurationContext Configuration { get; }
         INetStream<TKey, TMessage> Filter(Func<IConsumeContext<TKey, TMessage>, bool> filterPredicate);
         IHandle<TKey, TMessage, TResponseKey, TResponse> Handle<TResponseKey, TResponse>(Func<IConsumeContext<TKey, TMessage>, TResponse> handleConsumeContext);
+        IHandle<TKey, TMessage, TResponseKey, TResponse> HandleAsync<TResponseKey, TResponse>(Func<IConsumeContext<TKey, TMessage>, Task<TResponse>> handleConsumeContext);
         INetStream<TKey, TMessage> Handle(Action<IConsumeContext<TKey, TMessage>> handleConsumeContext);
+        INetStream<TKey, TMessage> HandleAsync(Func<IConsumeContext<TKey, TMessage>, Task> handleTask);
         Task StartAsync(CancellationToken token);
     }
 }
