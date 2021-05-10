5cff6361bef82b7d77b716271cf432f787199c4e
Phil Jerkins <Phil@TheJerkins.net>
2021-05-10 16:34:38 -0500

    feat: Added a couple more specs to round-out the functionality of ConsumePipeline

--- src/NetStream.Specs/Specifications/Component/ConsumePipelineSpecs.cs
+++ src/NetStream.Specs/Specifications/Component/ConsumePipelineSpecs.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Machine.Specifications;
 using Machine.Specifications.Model;
@@ -38,8 +39,59 @@ namespace NetStreams.Specs.Specifications.Component
 
             private It should_have_three_after_two = () =>
                 ((TestHandleStep<string, string>)_handle2.Next).Name.ShouldEqual("Handle3");
+        }
 
-            //private It should_have_two_in_log = () => _log.ShouldContain("Handle2");
+        [Subject("PrependStep")]
+        class when_prepending_multiple_steps
+        {
+            static ConsumePipeline<string, string> _pipeline;
+            static string _log = "";
+            static TestHandleStep<string, string> _handle1;
+            static TestHandleStep<string, string> _handle2;
+            static TestHandleStep<string, string> _handle3;
+
+            private Establish context = () =>
+            {
+                _pipeline = new ConsumePipeline<string, string>();
+                _handle1 = new TestHandleStep<string, string>("Handle1", context => _log += "Handle1 Succeeded; ");
+                _handle2 = new TestHandleStep<string, string>("Handle2", context => _log += "Handle2 Succeeded; ");
+                _handle3 = new TestHandleStep<string, string>("Handle3", context => _log += "Handle3 Succeeded; ");
+                _pipeline.PrependStep(_handle1);
+                _pipeline.PrependStep(_handle2);
+            };
+
+            Because of = () => _pipeline.PrependStep(_handle3);
+
+            private It should_have_one_after_two = () =>
+                ((TestHandleStep<string, string>)_handle2.Next).Name.ShouldEqual("Handle1");
+
+            private It should_have_two_after_three = () =>
+                ((TestHandleStep<string, string>)_handle3.Next).Name.ShouldEqual("Handle2");
+        }
+
+        [Subject("ExecuteAsync")]
+        class when_executing_the_pipeline
+        {
+            static ConsumePipeline<string, string> _pipeline;
+            static string _log = "";
+            static TestHandleStep<string, string> _handle1;
+            static TestHandleStep<string, string> _handle2;
+            static TestHandleStep<string, string> _handle3;
+
+            private Establish context = () =>
+            {
+                _pipeline = new ConsumePipeline<string, string>();
+                _handle1 = new TestHandleStep<string, string>("Handle1", context => _log += "Handle1 Succeeded; ");
+                _handle2 = new TestHandleStep<string, string>("Handle2", context => _log += "Handle2 Succeeded; ");
+                _handle3 = new TestHandleStep<string, string>("Handle3", context => _log += "Handle3 Succeeded; ");
+                _pipeline.AppendStep(_handle1);
+                _pipeline.AppendStep(_handle2);
+                _pipeline.AppendStep(_handle3);
+            };
+
+            private Because of = () => _pipeline.ExecuteAsync(new ConsumeContext<string, string>(null, null, null), CancellationToken.None).Wait();
+
+            private It should_have_Handle2_in_the_run_log = () => _log.IndexOf("Handle2").ShouldBeGreaterThan(-1);
         }
     }
 }
