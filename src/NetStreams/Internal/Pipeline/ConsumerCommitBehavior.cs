4666e0b72bb98d6aa5a2634c1e2abc64c9d35db8
Joel Brinkley <jvbrinkley21@gmail.com>
2021-07-21 07:48:38 -0500

    fix: Consume Commit Behavior not calling commit

--- src/NetStreams/Internal/Pipeline/ConsumerCommitBehavior.cs
+++ src/NetStreams/Internal/Pipeline/ConsumerCommitBehavior.cs
@@ -15,7 +15,10 @@ namespace NetStreams.Internal.Pipeline
 
         public override async Task<NetStreamResult> Execute(IConsumeContext<TKey, TMessage> consumeContext, CancellationToken token, NetStreamResult result = null)
         {
-            if (Next != null) return await Next.Execute(consumeContext, token, result);
+            if (Next != null)
+            {
+                result = await Next.Execute(consumeContext, token, result);
+            }
 
            _consumer.Commit();
 
