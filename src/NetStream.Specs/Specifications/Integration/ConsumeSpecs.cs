8512dfcb5fb1176169c7690b6b312ba6a5910876
Joel Brinkley <joel.brinkley@carecorenational.com>
2021-07-28 08:11:09 -0500

    merge commit

--- src/NetStream.Specs/Specifications/Integration/ConsumeSpecs.cs
+++ src/NetStream.Specs/Specifications/Integration/ConsumeSpecs.cs
@@ -89,7 +89,7 @@ namespace NetStreams.Specs.Specifications.Integration
             {
                 new TopicService().CreateAll(_sourceTopic, _destinationTopic);
 
-                _producer = new TestProducerService<string, TestMessage>(_sourceTopic);
+                _producer = Infrastructure.Services.TestProducerFactory.Plaintext<string, TestMessage>(_sourceTopic);
 
                 var firstTestMessage = new TestMessage();
                 var consumerGroup = Guid.NewGuid().ToString();
@@ -136,7 +136,7 @@ namespace NetStreams.Specs.Specifications.Integration
             {
                 new TopicService().CreateAll(_sourceTopic, _destinationTopic);
 
-                _producer = new TestProducerService<string, TestMessage>(_sourceTopic);
+                _producer = Infrastructure.Services.TestProducerFactory.Plaintext<string, TestMessage>(_sourceTopic);
 
                 _stream = new NetStreamBuilder<string, TestMessage>(cfg =>
                 {
@@ -178,8 +178,7 @@ namespace NetStreams.Specs.Specifications.Integration
             {
                 new TopicService().CreateAll(_sourceTopic, _destinationTopic);
 
-                _producer = new TestProducerService<string, TestMessage>(_sourceTopic);
-
+                _producer = Infrastructure.Services.TestProducerFactory.Plaintext<string, TestMessage>(_sourceTopic);
                 _stream = new NetStreamBuilder<string, TestMessage>(cfg =>
                         {
                             cfg.BootstrapServers = "localhost:9092";
@@ -204,6 +203,45 @@ namespace NetStreams.Specs.Specifications.Integration
             Because of = () => _producer.ProduceAsync(Guid.NewGuid().ToString(), new TestMessage()).BlockUntil(() => _consumedOffsets.Count > 3).Await();
 
             Cleanup after = () => _stream.Stop();
+            It should_complete_the_stream_task = () => _streamTask.Status.ShouldEqual(TaskStatus.RanToCompletion);
+        }
+
+        [Subject("Consume")]
+        class when_starting_a_stopped_stream
+        {
+            static readonly string _sourceTopic = $"r.{Guid.NewGuid()}";
+            static readonly string _destinationTopic = $"r.{Guid.NewGuid()}";
+            static TestProducerService<string, TestMessage> _producer;
+            static INetStream _stream;
+            static List<TestMessage> _consumedMessages = new List<TestMessage>();
+            static Task _streamTask;
+
+            Establish context = () =>
+            {
+                new TopicService().CreateAll(_sourceTopic, _destinationTopic);
+
+                _producer = Infrastructure.Services.TestProducerFactory.Plaintext<string, TestMessage>(_sourceTopic);
+
+                var firstTestMessage = new TestMessage();
+
+                var stream = DefaultBuilder.Plaintext<string, TestMessage>()
+                .Stream(_sourceTopic)
+                .Handle(context => _consumedMessages.Add(context.Message))
+                .ToTopic<string, TestMessage>(_destinationTopic)
+                .Build();
+
+                var streamTask = stream.StartAsync(CancellationToken.None);
+
+                _producer.ProduceAsync(Guid.NewGuid().ToString(), firstTestMessage).BlockUntil(() => _consumedMessages.Count == 1).Await();
+
+                stream.Stop();
+
+                streamTask.BlockUntil(() => streamTask.Status == TaskStatus.RanToCompletion).Await();
+
+                stream.StartAsync(CancellationToken.None);
+
+            };
+            Because of = () => _producer.ProduceAsync(Guid.NewGuid().ToString(), new TestMessage()).BlockUntil(() => _consumedMessages.Count == 2).Await();
 
             It should_retry_the_same_offset = () => _consumedOffsets.TrueForAll(x => x == 0);
         }
