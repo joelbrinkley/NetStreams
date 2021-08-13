c6c995e13c2459ecd23d11e8217a531568bbd765
Joel Brinkley <joel.brinkley@carecorenational.com>
2021-08-13 09:13:40 -0500

    add test for correlating messages when an id already exists

--- src/NetStream.Specs/Specifications/Integration/CorrelationSpecs.cs
+++ src/NetStream.Specs/Specifications/Integration/CorrelationSpecs.cs
@@ -23,7 +23,6 @@ namespace NetStreams.Specs.Specifications.Integration
             static string _sourceTopic = $"corr.src.{Guid.NewGuid()}";
             static string _destinationTopic = $"corr.dest.{Guid.NewGuid()}";
             static TestProducerService<string, TestMessage> _producer;
-            static INetStream _stream;
             static string _actualCorrelationId;
 
             Establish context = () =>
@@ -32,7 +31,7 @@ namespace NetStreams.Specs.Specifications.Integration
 
                 _producer = TestProducerMother.New<string, TestMessage>(_sourceTopic);
 
-                _stream = new NetStreamBuilder<string, TestMessage>(cfg =>
+                new NetStreamBuilder<string, TestMessage>(cfg =>
                 {
                     cfg.BootstrapServers = "localhost:9092";
                     cfg.ConsumerGroup = Guid.NewGuid().ToString();
@@ -41,8 +40,8 @@ namespace NetStreams.Specs.Specifications.Integration
                 })
                 .Stream(_sourceTopic)
                 .ToTopic<string, TestMessage>(_destinationTopic)
-                .Build();
-                _stream.StartAsync(CancellationToken.None);
+                .Build()
+                .StartAsync(CancellationToken.None);
 
                 DefaultBuilder.New<string, TestMessage>()
                 .Stream(_destinationTopic)
@@ -53,10 +52,66 @@ namespace NetStreams.Specs.Specifications.Integration
 
             Because of = () => _producer.ProduceAsync(Guid.NewGuid().ToString(), new TestMessage()).BlockUntil(() => _actualCorrelationId != null).Await();
 
-            Cleanup after = () => _stream.Stop();
-
             It should_be_published_with_a_correlation_id = () => _actualCorrelationId.ShouldNotBeEmpty();
         }
 
+        [Subject("Correlation")]
+        class when_a_message_with_a_correlation_id_is_consumed
+        {
+            static string _sourceTopic = $"correx.src.{Guid.NewGuid()}";
+            static string _destination1 = $"correx.dest1.{Guid.NewGuid()}";
+            static string _destination2 = $"correx.dest2.{Guid.NewGuid()}";
+            static TestProducerService<string, TestMessage> _producer;
+            static string _expectedCorrelationId;
+            static string _actualCorrelationId;
+
+            Establish context = () =>
+            {
+                new TopicService().CreateAll(_sourceTopic, _destination1, _destination2);
+
+                _producer = TestProducerMother.New<string, TestMessage>(_sourceTopic);
+
+                new NetStreamBuilder<string, TestMessage>(cfg =>
+                {
+                    cfg.BootstrapServers = "localhost:9092";
+                    cfg.ConsumerGroup = Guid.NewGuid().ToString();
+                    cfg.AutoOffsetReset = AutoOffsetReset.Earliest;
+                    cfg.EnableMessageCorrelation();
+                })
+                .Stream(_sourceTopic)
+                .ToTopic<string, TestMessage>(_destination1)
+                .Build()
+                .StartAsync(CancellationToken.None);
+
+                new NetStreamBuilder<string, TestMessage>(cfg =>
+                {
+                    cfg.BootstrapServers = "localhost:9092";
+                    cfg.ConsumerGroup = Guid.NewGuid().ToString();
+                    cfg.AutoOffsetReset = AutoOffsetReset.Earliest;
+                    cfg.EnableMessageCorrelation();
+                })
+               .Stream(_destination1)
+               .Handle(context => _expectedCorrelationId = context.Headers.FirstOrDefault(x => x.Key == CorrelationHeader.Value).Value)
+               .ToTopic<string, TestMessage>(_destination2)
+               .Build()
+               .StartAsync(CancellationToken.None);
+
+               new NetStreamBuilder<string, TestMessage>(cfg =>
+               {
+                   cfg.BootstrapServers = "localhost:9092";
+                   cfg.ConsumerGroup = Guid.NewGuid().ToString();
+                   cfg.AutoOffsetReset = AutoOffsetReset.Earliest;
+                   cfg.EnableMessageCorrelation();
+               })
+              .Stream(_destination2)
+              .Handle(context => _actualCorrelationId = context.Headers.FirstOrDefault(x => x.Key == CorrelationHeader.Value).Value)
+              .Build()
+              .StartAsync(CancellationToken.None);
+            };
+
+            Because of = () => _producer.ProduceAsync(Guid.NewGuid().ToString(), new TestMessage()).BlockUntil(() => _expectedCorrelationId != null).Await();
+
+            It should_be_published_with_the_consumed_message_correlation_id = () => _actualCorrelationId.ShouldEqual(_expectedCorrelationId);
+        }
     }
 }
